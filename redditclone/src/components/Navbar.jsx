ccaee9a05b213c410bbda190028101489c5a13e0
Jack12404 <jackli@computingforall.org>
2025-02-18 18:51:09 -0800

    start of nav bar

--- redditclone/src/components/Navbar.jsx
+++ redditclone/src/components/Navbar.jsx
@@ -1,6 +1,25 @@
+import logo from "../assets/CFA-logo.png";
 export default function Navbar() {
     return (
         <div>
+            <nav className = "flex justify-between pl-8 pr-8 pt-4 pb-4" >
+
+                <div className = "flex">
+                    <img className = "pr-8" src = { logo }/>
+                    <h1>Chatting For All</h1>
+                </div>
+                
+                <div className = "flex">
+                    <button className = "rounded-md pl-2 pr-2 pb-1 pt-1 bg-indigo-500"
+                        id="login"
+                        onClick={()=>login()}
+                        >
+                        Login / Sign up
+                    </button>
+                    <h1>User</h1>
+                </div>
+            </nav>
+
             {/*
             add logo
             login/logout buttons
