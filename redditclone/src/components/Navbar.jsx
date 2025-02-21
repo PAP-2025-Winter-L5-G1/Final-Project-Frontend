03849037d0c4f95b67de1982a5c72f4a05d3f01d
Jack12404 <jackli@computingforall.org>
2025-02-20 18:49:17 -0800

    nav bar and side bar

--- redditclone/src/components/Navbar.jsx
+++ redditclone/src/components/Navbar.jsx
@@ -1,31 +1,22 @@
 import logo from "../assets/CFA-logo.png";
 export default function Navbar() {
     return (
-        <div>
-            <nav className = "flex justify-between pl-8 pr-8 pt-4 pb-4" >
+            <nav className = "flex justify-between px-8 py-4 sticky top-0 bg-white" >
 
                 <div className = "flex">
                     <img className = "pr-8 max-h-15" src = { logo }/>
                     <h1 className="text-2xl pt-3">Chatting For All</h1>
                 </div>
                 
-                <div className = "flex  self-center">
-                    <button className = "mt-1 rounded-3xl bg-zinc-700 pl-3 pr-3 pt-2 pb-3 mb-5 text-xl text-white"
+                <div className = "flex self-center">
+                    <button className = "mt-1 rounded-3xl bg-zinc-700 px-5 pt-2 pb-3 mb-5 text-xl text-white"
                         id="login"
                         onClick={()=>login()}
                         >
-                        Login / Sign up {/* replace with logout if logged in */}
+                        Login{/* replace with logout if logged in */}
                     </button>
                     <h1 className="pl-10 mt-4 text-xl">UserName of a User@!!</h1>
                 </div>
             </nav>
-
-            {/*
-            add logo
-            login/logout buttons
-            username/profile
-            return to home page button
-            */}
-        </div>
     );
 }
--- redditclone/src/components/Sidebar.jsx
+++ redditclone/src/components/Sidebar.jsx
@@ -1,10 +1,49 @@
 export default function Sidebar () {
+    let communites = [
+        {communityLink: "xPage", communityName: "x"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "zPage", communityName: "z"},
+        {communityLink: "xPage", communityName: "x"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "zPage", communityName: "z"},
+        {communityLink: "xPage", communityName: "x"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "zPage", communityName: "z"},
+        {communityLink: "xPage", communityName: "x"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "zPage", communityName: "z"},
+        {communityLink: "xPage", communityName: "x"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
+        ];
     return (
-        <div>
-            {/*
-                community buttons (unread/read)
-                "join more!" button
-            */}
+        <div className="max-h-[calc(100vh-104px)] bg-zinc-700 w-75 rounded-tr-xl flex flex-col items-center gap-4 pt-4 overflow-y-scroll pb-6">
+
+            {communites.map((community) => {
+                return <CommunityButton communityLink={community.communityLink} communityName={community.communityName}/>
+            })}
         </div>
     );
+}
+
+function CommunityButton(props) {
+    return (
+        <a className = "min-h-10 w-65 text-xl rounded-xl bg-purple-50 text-center truncate px-5 justify-center pt-1"
+        href={props.communityLink}
+        >
+        {props.communityName}
+    </a>
+    );
+
 }
