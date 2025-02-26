3e25a137d158575afd7d574b816e4a5f69e7b1e3
Jack12404 <jackli@computingforall.org>
2025-02-25 18:06:07 -0800

    nav bar, side bar done, comment creation done

--- redditclone/src/pages/HomePage.jsx
+++ redditclone/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
+import CommentCard from "../components/CommentCard";
+import Navbar from "../components/Navbar";
+import NewComment from "../components/NewComment";
+import Sidebar from "../components/Sidebar";
+
 export default function HomePage() {
 
     return (
-        <main>
-            <h1 className="text-3xl font-bold underline"> Home page </h1>
+        <main className="max-h-screen">
+            {/* <Navbar/>
+            <Sidebar/> */}
+            <NewComment/>
+            {/* <h1 className="text-3xl font-bold underline"> Home page </h1> */}
         </main>
     );
     
