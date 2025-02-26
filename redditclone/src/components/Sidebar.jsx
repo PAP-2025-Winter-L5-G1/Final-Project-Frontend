1b0927a076de0102599d64e13369c663f4877c0b
Jack12404 <jackli@computingforall.org>
2025-02-25 16:31:53 -0800

    Finished sidebar and navbar

--- redditclone/src/components/Sidebar.jsx
+++ redditclone/src/components/Sidebar.jsx
@@ -28,11 +28,12 @@ export default function Sidebar () {
         {communityLink: "alphebetPage", communityName: "abcdefghijklmnopqrstuvwxyz"},
         ];
     return (
-        <div className="max-h-[calc(100vh-104px)] bg-zinc-700 w-75 rounded-tr-xl flex flex-col items-center gap-4 pt-4 overflow-y-scroll pb-6">
+        <div className="min-h-[calc(100vh-104px)] max-h-[calc(100vh-104px)] bg-zinc-700 w-75 rounded-tr-xl flex flex-col items-center gap-4 pt-4 pb-6 overflow-auto">
 
             {communites.map((community) => {
                 return <CommunityButton communityLink={community.communityLink} communityName={community.communityName}/>
             })}
+            <CommunityButton communityLink="joinMoreCommunities" communityName="join more!"/>
         </div>
     );
 }
