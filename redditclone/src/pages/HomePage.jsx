3305dbdf938aab69ffb2e6fdf78ec475c6b0a7d1
Sydneyvo <Sydneyvo911@gmail.com>
2025-03-11 15:50:52 -0700

    Made adjustments to backend server API

--- redditclone/src/pages/HomePage.jsx
+++ redditclone/src/pages/HomePage.jsx
@@ -10,17 +10,26 @@ import { useParams } from "react-router-dom";
 export default function HomePage() {
     const [posts, setPost] = useState([]);
     const params = useParams();
-    const fetchPostsByCommunity = async ()=>{
-        const result = await fetch("http://localhost:3000/posts/getposts/"+((params.communityId)?(params.communityId):(0)));
+    
+    const fetchPostsByCommunity = async () => {
+        const result = await fetch(`http://localhost:3000/posts?communityId=${params.communityId}`);
         setPost(await result.json());
     };
-    const fetchAllPosts = async()=>{
-        const allResult = await fetch("http://localhost:3000/posts/getposts");
+    
+    const fetchAllPosts = async () => {
+        const allResult = await fetch("http://localhost:3000/posts");
         setPost(await allResult.json());
     };
-    useEffect(()=>{
-        params.communityId==null?(fetchAllPosts()):(fetchPostsByCommunity())
-    }, [params.communityId])
+
+    
+    useEffect(() => {
+        if (params.communityId) {
+            fetchPostsByCommunity();
+        } else {
+            fetchAllPosts();
+        }
+    }, [params.communityId]);
+    
     return (
         <main>
             <Navbar/>
@@ -28,7 +37,20 @@ export default function HomePage() {
             <div className="flex flex-col overflow-hidden min-h-[calc(100vh-104px)] max-h-[calc(100vh-104px)] grid-flow-col">
                 <div className="fixed  min-w-230 max-w-300 max-h-180 min-h-180 bg-zinc-700 pr-5 pl-10 border-t-30 border-zinc-700 rounded-t-lg bottom-0 right-15 overflow-y-auto">
                     {params.communityId==null?([]):(<Modal Content={NewPost} buttonText={"Add Post"} st={true} communityId={params.communityId}/>)}
-                    {posts.map((post)=> (<PostCard username={post.postCreator} date={post.postDate} title={post.postHeader} body={post.postContent} postId={post.postId} key={post.postId}/>))} {/* GET COMMUNITY NAME/USERNAME */}
+                    {posts?.length > 0 ? (
+                        
+    posts.map((post) => (
+        <PostCard 
+            username={post.userId} 
+            date={new Date(post.postDate).toLocaleString()} 
+            title={post.postContent} 
+            key={post.postId} 
+        />
+    ))
+) : (
+    <p>No posts available</p>
+)}
+
                 </div>
             </div>
         </main>
